84607c4b501d4091af5bc1869b8b0371adf30870
JJ Style <style.jj@pm.me>
2021-02-06 13:10:35 +0000

    flask facade test and pipeline with make

--- flask/app/facades/post/naiive_post_manager.py
+++ flask/app/facades/post/naiive_post_manager.py
@@ -5,29 +5,33 @@ from app.data import post_response, posts_response, new_post_request
 
 class NaiivePostManager(PostManager):
     def __init__(self):
-        self.posts = [{"id": 1, "title": "post title", "body": "body of post"}]
+        self.posts = []
 
     def list_posts(self):
-        posts = [{**p, **{"uri": url_for("post_ep", id=p["id"])}} for p in self.posts]
-        return posts_response.dump(posts)
+        return posts_response.dump(self.posts)
 
     def get_post_by_id(self, id):
         p = next((p for p in self.posts if p["id"] == id), None)
         if not p:
             return "Not Found", 404
         else:
-            p = {**p, **{"uri": url_for("post_ep", id=p["id"])}}
             return post_response.dump(p)
 
     def create_post(self, req):
-        post = self.new_post_request.load(req)
+        post = new_post_request.load(req)
         ids = [p.id for p in self.posts]
-        self.posts.append({**post, **{"id": max(ids) + 1}})
+        if len(ids) == 0:
+            ids = [-1]
+        new_post = {
+            **post,
+            **{"id": max(ids) + 1, "slug": post["title"].replace(" ", "-").lower()},
+        }
+        self.posts.append(new_post)
+        return new_post, 201
 
     def get_post_by_slug(self, slug: str):
         p = next((p for p in self.posts if p.get("slug", "") == slug), None)
         if not p:
             return "Not Found", 404
         else:
-            p = {**p, **{"uri": url_for("post_ep", id=p["id"])}}
             return post_response.dump(p)
---
+++ flask/app/facades/post/test_post_manager.py
@@ -0,0 +1,42 @@
+import pytest
+import json
+
+from .naiive_post_manager import NaiivePostManager
+from .post_factory import PostFactory
+from .post_manager import PostManager
+
+
+@pytest.fixture(scope="module")
+def naiive_post_manager() -> PostManager:
+    f = (PostFactory()).set_type("NAIIVE")
+    return f.new()
+
+
+def test_list_no_posts_happy(naiive_post_manager: PostManager):
+    res = naiive_post_manager.list_posts()
+    assert type(res) == list
+    assert len(res) == 0
+
+
+def test_get_post_by_id_404(naiive_post_manager: PostManager):
+    res = naiive_post_manager.get_post_by_id("randomid_that_doesnt_exist")
+    assert 404 in res
+
+
+def test_create_post(naiive_post_manager: PostManager):
+    res = naiive_post_manager.create_post({"title": "my post", "body": "body of post"})
+    assert 201 in res
+
+
+def test_get_by_id(naiive_post_manager: PostManager):
+    res = naiive_post_manager.get_post_by_id(0)
+    assert type(res) == dict
+    assert res.get("title") == "my post"
+    assert res.get("body") == "body of post"
+
+
+def test_get_by_slug(naiive_post_manager: PostManager):
+    res = naiive_post_manager.get_post_by_slug("my-post")
+    assert type(res) == dict
+    assert res.get("title") == "my post"
+    assert res.get("body") == "body of post"
